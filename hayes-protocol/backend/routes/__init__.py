c7f05d8e469bb2f6382e5eabef19318ae0d9a001
Zeynep TANRIVERMİŞ <zeynep_tanrivermis@hotmail.com>
2026-04-22 14:41:12 +0300

    Added init.py for deployment

---
+++ hayes-protocol/backend/routes/__init__.py
@@ -0,0 +1,4 @@
+from . import chat
+from . import images
+
+__all__ = ["chat", "images"]
---
+++ hayes-protocol/backend/services/__init__.py
@@ -0,0 +1,4 @@
+from . import claude_service
+from . import dalle_service
+
+__all__ = ["claude_service", "dalle_service"]
